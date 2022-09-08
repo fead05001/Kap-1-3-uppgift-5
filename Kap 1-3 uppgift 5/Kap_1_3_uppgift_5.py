9dd2033fce3dbb81f5564c537bd57f9d5a0fa963
felix <felix@DESKTOP-SSJR4EI>
2022-09-08 17:41:49 +0200

    Add project files.

---
+++ Kap 1-3 uppgift 5/Kap_1_3_uppgift_5.py
@@ -0,0 +1,15 @@
+volym=int(input("Tankad volym (liter)="))
+pris=int(input("Pris per liter (kr)="))
+betala=volym*pris 
+
+
+
+print("*--------------------------*")
+print("*          KVITTO          *")
+print("*  Tankad volym ",volym," liter *")
+print("*  Pris per liter ",pris," kr  *")
+print("*  Betalt kronor ",betala,"     *")
+print("*                          *")
+print("*    Tack for besoket och  *")
+print("*    valkommen ater!       *")
+print("*--------------------------*")
